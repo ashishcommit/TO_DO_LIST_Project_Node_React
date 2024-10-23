3d266e682439ffcfddb97f682de4c3ed9ecf4706
ashishcommit <ashishgoswami1110@gmail.com>
2024-10-23 17:53:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Form from './components/Form';
+import Data from './components/Data';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/data" element={<Data />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
